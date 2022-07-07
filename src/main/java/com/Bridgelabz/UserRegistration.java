ae2625b4fdabeaaf925bf93845a7bd3071b0bae9
unknown <https://github.com/Pooja-bhaskaran/Day03.git>
2022-07-07 13:33:50 +0530

    UC3 email id is added

--- src/main/java/com/Bridgelabz/UserRegistration.java
+++ src/main/java/com/Bridgelabz/UserRegistration.java
@@ -5,6 +5,8 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     static final String TEST_NAME_PATTERN = "^[A-Z]{1}[a-z]{1,}$";
+    static final String  TEST_EMAIL_PATTERN = "^[A-Z a-z 0-9]+([.][A-Za-z0-9]+)@([a-z]+[.][a-z]{2,3})+([.][a-z]+)$";
+
 
     public boolean validateFirstName(String exp) {
         Pattern pattern = Pattern.compile(TEST_NAME_PATTERN);
@@ -15,6 +17,12 @@ public class UserRegistration {
     public boolean validateLastName(String exp) {
         Pattern pattern = Pattern.compile(TEST_NAME_PATTERN);
         Matcher matcher = pattern.matcher(exp);
+        return matcher.matches();
+    }
+
+    public boolean validateEmailID(String email){
+        Pattern pattern = Pattern.compile(TEST_EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
         return matcher.matches();
     }
 }
